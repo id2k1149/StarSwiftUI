48a2958aa23347b27ebd9ee90c0c3f25cff43b49
id2k1149 <id2k1149@gmail.com>
2023-04-16 07:57:09 -0400

    Initial Commit

---
+++ StarSwiftUI/StarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarSwiftUIApp.swift
+//  StarSwiftUI
+//
+//  Created by Max Franz Immelmann on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct StarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
