b839051abd5b79b3d3de0205650f5530aaa88747
id2k1149 <id2k1149@gmail.com>
2023-04-17 08:38:22 -0400

    5 points star

--- StarSwiftUI/ContentView.swift
+++ StarSwiftUI/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .frame(width: 300)
+            Star()
+                .fill(Color.orange)
+                .frame(width: 250)
+            // 6 points degrees(-30)
+                .rotationEffect(.degrees(54))
         }
-        .padding()
+    }
+}
+
+struct Star: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        let radius = min(rect.width, rect.height) / 2
+        
+        let starPoints = calculateStarPoints(center: center,
+                                             radius: radius,
+                                             numberOfPoints: 5)
+        
+        path.addLines(starPoints)
+        path.closeSubpath()
+        
+        return path
+    }
+    
+    private func calculateStarPoints(center: CGPoint,
+                                     radius: CGFloat,
+                                     numberOfPoints: Int) -> [CGPoint] {
+        let angle = CGFloat.pi * 2 / CGFloat(numberOfPoints * 2)
+        var points = [CGPoint]()
+        
+        for i in 0..<numberOfPoints*2 {
+            let isEven = i % 2 == 0
+            let r = isEven ? radius : radius * 0.4
+            let a = angle * CGFloat(i)
+            let x = center.x + r * cos(a)
+            let y = center.y + r * sin(a)
+            points.append(CGPoint(x: x, y: y))
+        }
+        
+        return points
     }
 }
 
